3625fd4940cf6fce095044483f77a5cf2a34ebeb
marcelotakayama <marcelo.takayama00@gmail.com>
2020-04-03 17:17:18 -0300

    Tipagem fraca do JS

---
+++ fundamentos/tipagemFraca.js
@@ -0,0 +1,10 @@
+// Dentro do JS, você tem muita liberdade para 'tipar' o código
+// ou seja, você não precisa definir o tipo da variável dentro do código
+
+let qualquer = 'Legal';
+console.log(qualquer);
+console.log(typeof qualquer);
+
+qualquer = 3.1415;
+console.log(qualquer);
+console.log(typeof qualquer);
