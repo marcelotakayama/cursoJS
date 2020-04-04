6a2baf11f659955f2ecfa85c9094ac764d00a8be
marcelotakayama <marcelo.takayama00@gmail.com>
2020-04-03 21:33:58 -0300

    Números, alguns cuidados

---
+++ fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,9 @@
+// Em JS, é possível se dividir um número por zero, ele aceita tranquilamente
+console.log(7 / 0);
+
+// Você pode fazer tbm operações como strings com números
+console.log("10" / 2);
+
+console.log('show' * 2);
+console.log(0.1 + 0.7);
+console.log((10.345).toFixed(2));
