44bf7fe4502096d9cfc8b48cbf5843d883692315
marcelotakayama <marcelo.takayama00@gmail.com>
2020-04-07 17:14:17 -0300

    Manipulando strings em JS

---
+++ fundamentos/strings.js
@@ -0,0 +1,8 @@
+const escola = "Mackenzie";
+
+console.log(escola.charAt(4));
+console.log(escola.charCodeAt(4));
+console.log(escola.substring(1));
+console.log(escola.substring(0, 4));
+console.log("Universidade ".concat(escola).concat('!'));
+console.log('Ana, Maria, Pedro'.split(","));
