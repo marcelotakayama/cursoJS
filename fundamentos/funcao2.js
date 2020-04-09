47f1f9e5e7a74ede1ecb76e901b33c8cef5872f6
marcelotakayama <marcelo.takayama00@gmail.com>
2020-04-09 07:28:23 -0300

    Básico de funções 2

---
+++ fundamentos/funcao2.js
@@ -0,0 +1,20 @@
+// armazenando uma função em uma variável
+
+const imprimirSoma = function(a, b) {
+    console.log(a + b)
+}
+
+imprimirSoma(2, 3)
+
+// Armazenando uma função arrow em uma variável
+
+const soma = (a, b) => {
+    return a + b
+}
+
+console.log(soma(2, 4))
+
+// Retorno implicito
+
+const subtracao = (a, b) => a - b
+console.log(subtracao(3, 2))
