cc7338ad7ef4d15d5cc5280a26b3fddc7abc0a74
marcelotakayama <marcelo.takayama00@gmail.com>
2020-04-09 05:49:09 -0300

    Básico de funções 1

---
+++ fundamentos/funcao1.js
@@ -0,0 +1,19 @@
+// Função sem Retorno
+
+function imprimirSoma(a, b) {
+    console.log(a + b)
+}
+
+imprimirSoma(2, 3)
+imprimirSoma(2)
+imprimirSoma(3, 4, 5, 6, 7, 8, 9)
+
+// Função com retorno
+
+function soma(a, b = 0) {
+    return a + b
+}
+
+console.log(soma(2, 3))
+console.log(soma(2))
+console.log(soma())
