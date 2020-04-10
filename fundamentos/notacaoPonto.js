4f915c39298570925a4abea515c2bcf37bb8dd26
marcelotakayama <marcelo.takayama00@gmail.com>
2020-04-10 19:04:51 -0300

    Notação Ponto

---
+++ fundamentos/notacaoPonto.js
@@ -0,0 +1,23 @@
+// THIS serve para que a variável seja visível para todo o código e não só no escopo da função
+
+console.log(Math.ceil(6.1))
+
+const obj1 = {}
+obj1.nome = 'Bola'
+
+console.log(obj1.nome)
+
+function Obj(nome) {
+    this.nome = nome
+    this.exec = function() {
+        console.log("exec...")
+    }
+}
+
+const obj2 = new Obj('Cadeira')
+const obj3 = new Obj('Mesa')
+
+console.log(obj2.nome)
+console.log(obj3.nome)
+
+obj2.exec()
