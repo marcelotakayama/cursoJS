6d91ed3fc967eeb88e2e8eb8c4f9daea16eb30c4
marcelotakayama <marcelo.takayama00@gmail.com>
2020-04-10 04:29:40 -0300

    Usando let em loop 1

---
+++ fundamentos/usandoLetEmLoop1.js
@@ -0,0 +1,8 @@
+for (let i = 0; i < 10; i++) {
+    console.log(i)
+}
+
+console.log('i = ', i);
+
+// NESTE EXEMPLO ELE DA ERRO POIS QUANDO VC CONFIGURA UMA VARIÁVEL COM LET,
+// ELA SÓ SERÁ VISÍVEL DENTRO DO BLOCO DE CÓDIGO EM QUE ELA ESTÁ
