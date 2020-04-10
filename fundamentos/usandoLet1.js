344b858ccd31ac93849788987778a4a8293aa543
marcelotakayama <marcelo.takayama00@gmail.com>
2020-04-10 04:22:07 -0300

    Usando variáveis com let 1

---
+++ fundamentos/usandoLet1.js
@@ -0,0 +1,9 @@
+var numero = 1
+{
+    let numero = 2
+    console.log('Dentro = ', numero)
+}
+console.log("Fora = ", numero)
+
+// VAR = ESCOPO GLOBAL
+// LET = ESCOPO GLOBAL, ESCOPO DE FUNÇÃO E ESCOPO DE BLOCO
