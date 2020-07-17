6a5d4d937e78d54c96b9ec83453d4814eb0ffcca
marcelotakayama <marcelo.takayama00@gmail.com>
2020-07-17 02:16:18 -0300

    Outra forma de declarar funções

---
+++ funcao/tiposDeclaracao.js
@@ -0,0 +1,18 @@
+// Function Declaration
+function soma(x, y) {
+    return x+y
+}
+
+// function expression
+const sub = function(x, y) {
+    return x-y
+}
+
+// named function expression
+const mult = function mult(x, y) {
+    return x*y
+}
+
+console.log(soma(3, 4));
+console.log(sub(4, 3));
+console.log(mult(3, 4));
