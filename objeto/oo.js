27b4726080384666c9a0728b8d3c4189632c7088
marcelotakayama <marcelo.takayama00@gmail.com>
2020-11-05 16:15:54 -0300

    subido aula de OO

--- objeto/oo.js
+++ objeto/oo.js
@@ -1,2 +1,15 @@
 // Código não executável
 
+// Procedural
+processamento(valor1, valor2, valor3)
+
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        //
+    }
+}
+
+objeto.processamento()
